5cfd1c0e69e2bb481b92dad44f5cf9a7df55c666
kdle15 <kdle15@stlawu.edu>
2019-03-16 21:54:07 -0400

    Glide test work

--- app/src/main/java/com/example/mangaglide/MainActivity.java
+++ app/src/main/java/com/example/mangaglide/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.mangaglide;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Initiate fragment
+ */
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
